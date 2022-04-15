1e0e930767e63ca8567b4a444feddbec2457c406
Jesse Ruhl <60628685+jesseruhlz@users.noreply.github.com>
2022-04-15 15:08:00 -0700

    init commit
    init commit

---
+++ TeslaSuit_Test/Assets/Scripts/TeslaSuitPackage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeslasuitAPI.Utils;
+
+public class TeslaSuitPackage : MonoBehaviour
+{
+    //static void TeslasuitAPI.Teslasuit.Load();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
